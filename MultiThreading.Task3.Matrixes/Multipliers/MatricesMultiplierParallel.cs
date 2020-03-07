5c4760677d0eff6d05f78bb62a68496bf4c35587
Dmitriy Nenilin <Dmitriy_Nenilin@epam.com>
2020-03-07 16:04:10 +0300

    Parallel for for matrix muliplition

--- MultiThreading.Task3.Matrixes/Multipliers/MatricesMultiplierParallel.cs
+++ MultiThreading.Task3.Matrixes/Multipliers/MatricesMultiplierParallel.cs
@@ -1,13 +1,30 @@
-﻿using MultiThreading.Task3.MatrixMultiplier.Matrices;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using MultiThreading.Task3.MatrixMultiplier.Matrices;
 
 namespace MultiThreading.Task3.MatrixMultiplier.Multipliers
 {
-    public class MatricesMultiplierParallel : IMatricesMultiplier
-    {
-        public IMatrix Multiply(IMatrix m1, IMatrix m2)
-        {
-            // todo: feel free to add your code here
-            return new Matrix(1, 1);
-        }
-    }
+	public class MatricesMultiplierParallel : IMatricesMultiplier
+	{
+		public IMatrix Multiply(IMatrix m1, IMatrix m2)
+		{
+			var resultMatrix = new Matrix(m1.RowCount, m2.ColCount);
+
+			Parallel.For(0, m1.RowCount, i =>
+			{
+				for (var j = 0; j < m2.ColCount; j++)
+				{
+					long sum = 0;
+					for (var k = 0; k < m1.ColCount; k++)
+					{
+						sum += m1.GetElement(i, k) * m2.GetElement(k, j);
+					}
+
+					resultMatrix.SetElement(i, j, sum);
+				}
+			});
+
+			return resultMatrix;
+		}
+	}
 }

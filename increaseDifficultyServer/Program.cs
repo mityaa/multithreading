aaeb6849fa0458a79ea34ef10889fe733d34051d
Dmitriy Nenilin <Dmitriy_Nenilin@epam.com>
2020-03-07 16:53:03 +0300

    Started chat implementation (client)

---
+++ increaseDifficultyServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace increaseDifficultyServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ increaseDifficultyServer/client/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace increaseDifficulty.client
+{
+	public class Client
+	{
+		public string UserName { get; set; }
+		public string Id { get; private protected set; }
+		protected internal NetworkStream Stream { get; private protected set; }
+		public TcpClient TcpClient { get; set; }
+
+		public Client(TcpClient client)
+		{
+			Id = Guid.NewGuid().ToString();
+			TcpClient = client;
+		}
+
+		protected internal void CloseConnection()
+		{
+			Stream?.Close();
+			TcpClient?.Close();
+		}
+
+
+	}
+}

8d88a24b8f0b5256dca87ac96e4ba336ddb0f84c
Dmitriy Nenilin <Dmitriy_Nenilin@epam.com>
2020-03-07 17:33:06 +0300

    Server and client implemented

--- increaseDifficultyServer/client/Client.cs
+++ increaseDifficultyServer/client/Client.cs
@@ -1,29 +1,84 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Net.Sockets;
 using System.Text;
+using increaseDifficulty.server;
 
 namespace increaseDifficulty.client
 {
 	public class Client
 	{
 		public string UserName { get; set; }
-		public string Id { get; private protected set; }
-		protected internal NetworkStream Stream { get; private protected set; }
-		public TcpClient TcpClient { get; set; }
+		protected internal string Id;
+		protected internal NetworkStream Stream;
+		private readonly TcpClient _tcpClient;
+		private readonly Server _server;
 
-		public Client(TcpClient client)
+		public Client(TcpClient client, Server server)
 		{
 			Id = Guid.NewGuid().ToString();
-			TcpClient = client;
+			_tcpClient = client;
+			_server = server;
+			server.AddConnection(this);
 		}
 
 		protected internal void CloseConnection()
 		{
 			Stream?.Close();
-			TcpClient?.Close();
+			_tcpClient?.Close();
 		}
 
+		public void Process()
+		{
+			try
+			{
+				Stream = _tcpClient.GetStream();
+				var message = GetMessage();
+				UserName = message;
 
+				message = UserName + "now in chat";
+				_server.SendMessage(message, Id);
+				Console.WriteLine(message);
+
+				while (true)
+				{
+					try
+					{
+						message = GetMessage();
+						message = $"{UserName}: {message}";
+						Console.WriteLine(message);
+						_server.SendMessage(message, Id);
+					}
+					catch
+					{
+						message = $"{UserName} left the chat";
+						Console.WriteLine(message);
+						_server.SendMessage(message, Id);
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine(e.Message);
+			}
+			finally
+			{
+				_server.RemoveConnection(Id);
+				CloseConnection();
+			}
+		}
+
+		private string GetMessage()
+		{
+			var data = new byte[64];
+			var stringBuilder = new StringBuilder();
+
+			do
+			{
+				var bytes = Stream.Read(data, 0, data.Length);
+				stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+			} while (Stream.DataAvailable);
+
+			return stringBuilder.ToString();
+		}
 	}
 }
---
+++ increaseDifficultyServer/server/Server.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using increaseDifficulty.client;
+
+namespace increaseDifficulty.server
+{
+	public class Server
+	{
+		protected static TcpListener TcpListener;
+		protected List<Client> Clients = new List<Client>();
+
+		protected internal void AddConnection(Client client)
+		{
+			Clients.Add(client);
+		}
+
+		protected internal void RemoveConnection(string id)
+		{
+			var client = Clients.FirstOrDefault(c => c.Id == id);
+			if (client != null)
+				Clients.Remove(client);
+		}
+
+		protected internal void Listen()
+		{
+			try
+			{
+				TcpListener = new TcpListener(IPAddress.Any, 8484);
+				TcpListener.Start();
+				Console.WriteLine("Server started and waiting");
+				while (true)
+				{
+					var tcpClient = TcpListener.AcceptTcpClient();
+
+					var client = new Client(tcpClient, this);
+					var clientThread = new Thread(new ThreadStart(client.Process));
+					clientThread.Start();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+				DropAllConnections();
+			}
+		}
+
+		protected internal void SendMessage(string message, string id)
+		{
+			var data = Encoding.Unicode.GetBytes(message);
+			foreach (var client in Clients.Where(client => client.Id != id))
+			{
+				client.Stream.Write(data, 0, data.Length);
+			}
+		}
+
+		protected internal void DropAllConnections()
+		{
+			TcpListener.Stop();
+
+			foreach (var client in Clients)
+			{
+				client.CloseConnection();
+			}
+
+			Environment.Exit(0);
+		}
+	}
+}
